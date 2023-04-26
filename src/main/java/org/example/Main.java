14d4212ea88b47ab7338c925ce23126a262753aa
rachelcynthia <rachel883omega@gmail.com>
2023-04-26 10:26:45 +0530

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/external-service", (exchange -> {
+
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String responseText = "Response from external service";
+                try {
+                    TimeUnit.SECONDS.sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                exchange.sendResponseHeaders(200, responseText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(responseText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        }));
+
+
+        server.setExecutor(null); // creates a default executor
+        server.start();
+
+    }
+
+}
